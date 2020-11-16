716b019d4738b25e5430c4619003d4cb0decedd4
Ritvik Gupta <ritvikgupta199@gmail.com>
2020-11-16 22:29:34 +0530

    Added Comments in A1List

--- A1List.java
+++ A1List.java
@@ -21,11 +21,15 @@ public class A1List extends List {
     }
 
     public A1List Insert(int address, int size, int key) {
+        // Inserting a new node after the current node in the DLL
+        // Creating a new node
         A1List node = new A1List(address, size, key);
 
+        // Linking new node with the node next to current node
         node.next = this.next;
         this.next.prev = node;
 
+        // Linking new node with the current node
         this.next = node;
         node.prev = this;
 
@@ -34,12 +38,14 @@ public class A1List extends List {
 
     public boolean Delete(Dictionary d) {
         A1List v = this;
+        // Finding the node with match in the forward direction
         while (v != null) {
             if (v.key == d.key && v.address == d.address && v.size == d.size) {
                 break;
             }
             v = v.next;
         }
+        // If the node is not found, then find the match in the backward direction
         if (v == null) {
             v = this;
             while (v != null) {
@@ -49,9 +55,11 @@ public class A1List extends List {
                 v = v.prev;
             }
         }
+
         if (v == null)
             return false;
         else {
+            // Removing the node found
             v.next.prev = v.prev;
             v.prev.next = v.next;
             return true;
@@ -60,12 +68,14 @@ public class A1List extends List {
 
     public A1List Find(int k, boolean exact) {
         A1List v = this;
+        // Finding the node with match in the forward direction
         while (v != null) {
             if ((exact && v.key == k) || (!exact && v.key >= k)) {
                 break;
             }
             v = v.next;
         }
+        // If the node is not found, then find the match in the backward direction
         if (v == null) {
             v = this;
             while (v != null) {
@@ -80,6 +90,7 @@ public class A1List extends List {
 
     public A1List getFirst() {
         A1List v = this;
+        // Iterate to the head node of the list
         while (v.prev != null) {
             v = v.prev;
         }
@@ -92,21 +103,29 @@ public class A1List extends List {
 
     public boolean sanity() {
         A1List v = this.getFirst();
+        // Check if Head node has all values as -1 and prev pointer as null
         if (!(v.prev == null && v.key == -1 && v.address == -1 && v.size == -1))
             return false;
         v = v.next;
+
+        // Checking for invariants for each node
         while (v.next != null) {
             if (v.next.prev != v || v.prev.next != v)
                 return false;
             v = v.next;
         }
+
+        // Check if Tail node has all values as -1 and next pointer as null
         if (!(v.next == null && v.key == -1 && v.address == -1 && v.size == -1))
             return false;
+        
+        // Checking if the list gets circular using two-pointer method
         A1List p1 = this.getFirst();
         A1List p2 = this.getFirst();
         while (p1 != null && p2 != null && p2.next != null) {
             p1 = p1.next;
             p2 = p2.next.next;
+            // If there is a match then there exists a loop in the list
             if (p1 == p2) {
                 return false;
             }
